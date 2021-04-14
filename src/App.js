733003644355265694254a5a508f235bccf672cf
simbox <simon@aurora.tech>
2021-04-14 14:39:27 -0700

    Added a navbar and page structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from 'react-bootstrap';
+import BootstrapNavbar from './bootsrapNavBar.js';
 
 function App() {
   return (
     <div className="App">
+      <BootstrapNavbar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Simons New Website</h1>
