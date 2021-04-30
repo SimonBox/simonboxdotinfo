999cb033187eb9d27b37fadd7f22247b25baef63
simbox <simon@aurora.tech>
2021-04-29 20:37:52 -0700

    modified menu styling

--- src/home.js
+++ src/home.js
@@ -7,10 +7,10 @@ class Homepage extends React.Component{
     content(){
         return(
             <div>
-                <h1>Raspberry Pi - iTelescope</h1>
+                <h2>Raspberry Pi - iTelescope</h2>
                 <p>The latest updates to this project to instrument a Sky-Watcher Dobsonian telescope include the adoption of a Raspberry Pi as the telescope's computer, bluetooth connection to a laptop and a user interface software on the laptop for set up and calibration.</p>
                     <iframe width="480" height="360" src="//www.youtube-nocookie.com/embed/fRAF6LnHk1A?rel=0" frameborder="0" allowfullscreen=""></iframe>
-                <h2>Software</h2>
+                <h3>Software</h3>
                 <ul> 
                     <li><b>Python Programme for the Raspberry Pi: </b><a class="page" href="http://sourceforge.net/projects/itelescope/files/?source=navbar">iTelRaspberry (SourceForge redirect)</a></li>
                     <li><b>Python Programme for the Laptop: </b><a class="page" href="http://sourceforge.net/projects/itelescope/files/?source=navbar">iTelComputer (SourceForge redirect)</a></li>
